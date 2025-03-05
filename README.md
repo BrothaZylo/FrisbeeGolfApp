41235b6af489b26e96e25b184b42f352c4c67f44
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2025-03-05 01:11:18 +0100

    load and play video
    Added function to load video and play it

---
+++ README.md
@@ -0,0 +1,2 @@
+# Frisbeegolf
+ 
---
+++ tracker.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import numpy
+
+
+# https://stackoverflow.com/questions/36521449/how-to-load-a-video-in-opencvpython
+def load_video(video):
+    # Load video file 
+    cap = cv.VideoCapture(video)
+    # TODO Make something to check if the video is able to open
+    return cap
+
+def load_and_play_video(video):
+    # https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
+    cap = cv.VideoCapture(video)
+     
+    while cap.isOpened():
+        ret, frame = cap.read()
+    
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+
+
+
+def extract_file():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    
